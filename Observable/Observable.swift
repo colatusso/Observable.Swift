ddd913fd394d87949a359c2a398c92045fbd1dd1
Rafael <rafael@pi.local>
2017-02-13 17:52:05 -0200

    - new hability to select the bool state to run the closure - new property to add as much switches as you need

--- Observable/Observable.swift
+++ Observable/Observable.swift
@@ -1,14 +1,24 @@
 import UIKit
 
+enum BoolState {
+    case AnyState
+    case OnlyTrue
+    case OnlyFalse
+}
+
 class Observable<T> {
-    var valueDidChange:(()->())?
-    fileprivate var _switch: ObservableSwitch?
+    fileprivate var _switches: [ObservableSwitch] = []
     fileprivate var _value: T? = nil
+    var valueDidChange:(()->())?
     
     var value: T {
         set {
             _value = newValue
-            _switch?.validate()
+            
+            for s in self._switches {
+                s.validate()
+            }
+            
             valueDidChange?()
         }
         get {
@@ -21,18 +31,25 @@ class Observable<T> {
     }
     
     func addSignal(_ signal: @escaping () -> Bool, toSwitch: ObservableSwitch) {
-        self._switch = toSwitch
-        self._switch?.addSignal(signal)
+        for s in self._switches where s === toSwitch {
+            s.addSignal(signal)
+            
+            return
+        }
+        
+        toSwitch.addSignal(signal)
+        self._switches.append(toSwitch)
     }
 }
 
 class ObservableTextField: UITextField, UITextFieldDelegate {
+    fileprivate var _switches: [ObservableSwitch] = []
     var textDidChange:((_ text: String)->())?
-    fileprivate var _switch: ObservableSwitch?
     var count = 0
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
+        self.addTarget(self, action: #selector(textFieldDidChange(_:)), for: .editingChanged)
         delegate = self
     }
     
@@ -47,21 +64,31 @@ class ObservableTextField: UITextField, UITextFieldDelegate {
                 count = text!.characters.count + string.characters.count
                 textDidChange?(text! + string)
         }
-        
-        _switch?.validate()
 
         return true
     }
     
+    func textFieldDidChange(_ textField: UITextField) {
+        for s in self._switches {
+            s.validate()
+        }
+    }
+    
     func addSignal(_ signal: @escaping () -> Bool, toSwitch: ObservableSwitch) {
-        self._switch = toSwitch
-        self._switch?.addSignal(signal)
+        for s in self._switches where s === toSwitch {
+            s.addSignal(signal)
+            
+            return
+        }
+        
+        toSwitch.addSignal(signal)
+        self._switches.append(toSwitch)
     }
 }
 
 class ObservableTextView: UITextView, UITextViewDelegate {
+    fileprivate var _switches: [ObservableSwitch] = []
     var textDidChange:((_ text: String) -> ())?
-    fileprivate var _switch: ObservableSwitch?
     
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)!
@@ -69,32 +96,56 @@ class ObservableTextView: UITextView, UITextViewDelegate {
     }
     
     func textViewDidChange(_ textView: UITextView) {
-        _switch?.validate()
+        for s in self._switches {
+            s.validate()
+        }
+        
         textDidChange?(textView.text)
     }
     
     func addSignal(_ signal: @escaping () -> Bool, toSwitch: ObservableSwitch) {
-        self._switch = toSwitch
-        self._switch?.addSignal(signal)
+        for s in self._switches where s === toSwitch {
+            s.addSignal(signal)
+            
+            return
+        }
+        
+        toSwitch.addSignal(signal)
+        self._switches.append(toSwitch)
     }
 }
 
 class ObservableSwitch {
     fileprivate var signals: [() -> Bool] = []
+    fileprivate var boolState: BoolState
     var action:((_ status: Bool)->())?
+    
+    init(_ boolState: BoolState) {
+        self.boolState = boolState
+    }
     
     fileprivate func addSignal(_ signal: @escaping () -> Bool) {
         self.signals.append(signal)
     }
     
     fileprivate func validate() {
+        var result = true
+        
         for check in signals {
-            if !check() {
-                action?(false)
-                return
+            if (result && !check()) {
+                result = false
+                break
             }
         }
         
-        action?(true)
+        if boolState == .AnyState {
+            action?(result)
+        }
+        else if boolState == .OnlyTrue && result {
+            action?(true)
+        }
+        else if boolState == .OnlyFalse && !result {
+            action?(false)
+        }
     }
 }
